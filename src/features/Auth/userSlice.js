19af6d4cd8aa9e8752db505256fb86e8c8792f1a
Son Doan <doansonbn24@gmail.com>
2022-03-01 10:21:49 +0700

    async action register

---
+++ src/features/Auth/userSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from 'api/userApi';
+
+const register = createAsyncThunk('users/register', async (payload) => {
+  // Call API register
+  const data = await userApi.register(payload);
+
+  // Save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  // Return user data
+  return data.user;
+});
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    current: {},
+    settings: {},
+  },
+  reducers: {},
+  extraReducers: {
+    // user/register/fullfilled
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+  },
+});
+
+const { reducer } = userSlice;
+export default reducer;
